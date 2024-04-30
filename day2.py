07b1105ff46452a42947b929d729d0c7b83af17a
Pierina <110428224+pierinak@users.noreply.github.com>
2024-04-30 00:50:46 -0300

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,11 @@
+print ("Getting to know you !")
+print ("""Esse é o dia 2 do desafio, faltam 98 ainda! Neste código vamos basicamente implementar alguns Inputs e Outputs sobre o usuário X. \n""")
+
+name = input("What's your name?")
+food = input("What's your favourite food?")
+music = input("What's your favourite music?")
+location = input("Where do you live?")
+
+print ("You are")
+print (name)
+print ("You're probably hungry for",food, "and you're definitely getting your groove on to",music, "living in the amazing",location)
