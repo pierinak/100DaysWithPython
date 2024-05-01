0a252f3892a350596cc3e899298d4e4007b07567
Pierina <110428224+pierinak@users.noreply.github.com>
2024-04-30 21:56:46 -0400

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,9 @@
+food = input ("What is your favorite food?")
+plant = input ("What is your favorite plant?")
+MethodOfCooking = input ("What is your favorite method os cooking?")
+BurnedFood = input ("What is your favorite word to describe burned food?")
+householdItem = input ("What is your favorite household item?")
+cookingType = input("What is a way to cook something? ")
+
+print ("Tonight's dinner:")
+print("For dinner you should make", cookingType, food, "with", BurnedFood, plant, "on a plate of", householdItem)
