75e1c77ff7a296a07f894e88cc2cadd04a03bf68
Pierina <110428224+pierinak@users.noreply.github.com>
2024-05-09 22:19:59 -0400

    day8.py

---
+++ day8.py
@@ -0,0 +1,8 @@
+# Solicitação de entrada do usuário
+idade = int(input("Qual é a sua idade? "))
+
+# Verifica se a idade é maior ou igual a 18
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
