f1ee6230fc5bccff2f30e1c6787fda173dfcbec6
Pierina <110428224+pierinak@users.noreply.github.com>
2024-05-26 14:52:26 -0300

    Update day8.py
    i just created a generator that creats positive phases

--- day8.py
+++ day8.py
@@ -1,25 +1,30 @@
-def get_user_age():
-    while True:
-        try:
-            idade = int(input("Qual é a sua idade? "))
-            if idade < 0:
-                raise ValueError("A idade não pode ser um número negativo.")
-            return idade
-        except ValueError as e:
-            print("Entrada inválida. Por favor, insira um número inteiro válido.")
-            print(f"Detalhes do erro: {e}")
+print ("Positive Generattor")
+print ("--------------------")
+print ()
+name = input ("What is your name? ")
+dayOfWeek = input ("What day of the week is it? ")
+favFood = input ("What is your favorite food? ")
+favColor = input ("What is your favorite color? ")
 
-def check_age(idade):
-    if idade >= 60:
-        print("Você é idoso.")
-    elif idade >= 18:
-        print("Você é maior de idade.")
-    else:
-        print("Você é menor de idade.")
-
-def main():
-    idade = get_user_age()
-    check_age(idade)
-
-if __name__ == "__main__":
-    main()
+if dayOfWeek == "Monday" or dayOfWeek == "monday":
+  print (name, "you are a monkey. You will have a great day today. Eat your favorite food:", favFood, "!")
+  
+elif dayOfWeek == "Tuesday" or dayOfWeek == "tuesday":
+  print (name, "you are a tiger. You will have a great day today. You will eat", favFood, "and wear", favColor, "today.")
+  
+elif dayOfWeek == "Wednesday" or dayOfWeek == "wednesday":
+  print (name, "you are a wolf. You will have a great day today. You will eat", favFood,)
+  
+elif dayOfWeek == "Thursday" or dayOfWeek == "thursday":
+  print (name, "you are a rabbit. You will have a great day today. You will eat", favFood, "and wear", favColor, "today.")
+  
+elif dayOfWeek == "Friday" or dayOfWeek == "friday":
+  print (name, "you are a dragon. You will have a great day today. You will eat", favFood, "wear your favorite color and hang out or do something you like!")
+  
+elif dayOfWeek == "Saturday" or dayOfWeek == "saturday":
+  print (name, "you are a cat. You will have a great day today. You will eat", favFood,"do something fun. Go out and enjoy yourself!")
+  
+elif dayOfWeek == "Sunday" or dayOfWeek == "sunday":
+  print (name,"Is the firts day of week! You are a dog. Try to enjoy your own company and relax a little! You are awesome and diserve that.")
+else :
+  print ("Even not knowing what day of the week it is, you are awesome! Enjeoy your day and try to retype the informations!")
