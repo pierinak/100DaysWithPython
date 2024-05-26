b44e4c93ee34782188ba34bbdf2f544fc79c2ce9
Pierina <110428224+pierinak@users.noreply.github.com>
2024-05-26 15:27:20 -0300

    Create Day9.py
    Just a simple code to show some message about you

---
+++ Day9.py
@@ -0,0 +1,14 @@
+print ("Generation Generator")
+print ()
+year = int (input ("What year were you born? "))
+if year >= 1925 and year <= 1946:
+  print ("You are a Traditionalist! ")
+elif year >= 1947 and year <= 1964:
+  print ("You are a Baby Boomer! ")
+elif year >= 1965 and year <= 1981:
+  print ("You are a Generation X! ")
+elif year >= 1982 and year <= 1995:
+  print ("You are a Millenial! ")
+elif year >= 1996 and year <= 2015:
+  print ("You are a Generation Z! ")
+  
